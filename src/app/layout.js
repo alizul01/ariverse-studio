4f37986a28518386b394ced21ad8c3523dcd238b
alizul01 <alizulfikar032@gmail.com>
2025-01-24 07:51:30 +0700

    update: favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,10 +23,15 @@ export default function RootLayout({children}) {
         <html lang="en">
         <head>
             <meta name="author" content={data_seo.author}/>
-            <link rel="icon" href="/images/favicon.ico" sizes="any"/>
             <title>
                 {data_seo.header}
             </title>
+            <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96"/>
+            <link rel="icon" type="image/svg+xml" href="/favicon.svg"/>
+            <link rel="shortcut icon" href="/favicon.ico"/>
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+            <meta name="apple-mobile-web-app-title" content="Ariverse"/>
+            <link rel="manifest" href="/site.webmanifest"/>
         </head>
         <body className={`${openSans.variable} ${poppins.variable}`}>
         <Header/>
